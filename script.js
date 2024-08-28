2c058fb14ef919bd4578f048120e759db955dd43
paiva737 <rafaelpaiva636@gmail.com>
2024-08-28 20:31:39 -0300

    Melhora do game, dando dicas de maior ou menor

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-let randomNumber = Math.round(Math.random() * 10)
+let randomNumber = Math.round(Math.random() * 100 + 1)
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector( "#btnReset")
 let xAttempts = 1
@@ -15,19 +15,31 @@ document.addEventListener('keydown', function(e){
     }
 })
 
+function exibirDicaNaTela(tag, texto){
+    let campo = document.querySelector(tag)
+    campo.innerHTML = texto
+}
+
+
 function handleTryClick(event){
     event.preventDefault()
-
+    
     const inputNumber = document.querySelector("#inputNumber")
-
-    if(inputNumber.value < 0 || inputNumber.value > 10){
-        alert('Apenas numeros entre 0 e 10')
+    if(inputNumber.value < 0 || inputNumber.value > 100){
+        alert('Apenas numeros entre 0 e 100')
     }
 
 if(Number(inputNumber.value) == randomNumber){
     toggleScreen()
-    screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
-   
+    let palavraTentativa = xAttempts > 1 ? 'Tentativas' : 'Tentativa'
+    screen2.querySelector("h2").innerText = `acertou em ${xAttempts} ${palavraTentativa}`
+} else{
+if(inputNumber.value > randomNumber){
+    exibirDicaNaTela('p', 'O numero secreto é menor')
+}else{
+    exibirDicaNaTela('p', 'O numero secreto é maior')
+}
+
 }
 inputNumber.value = ""
   xAttempts++
@@ -37,7 +49,7 @@ inputNumber.value = ""
 function handleResetClick(){
     toggleScreen()
     xAttempts = 1
-    randomNumber = Math.round(Math.random() * 10)
+    randomNumber = Math.round(Math.random() * 100 + 1)
 }
 
 
@@ -47,3 +59,4 @@ function toggleScreen(){
     screen1.classList.toggle("hide")
     screen2.classList.toggle("hide")
 }
+
